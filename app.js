d05119374e5b235dd97b4613c9c38e5d2d6c22b8
unknown <devethancraig@gmail.com>
2019-01-09 11:10:49 -0700

    GraphQl API Started

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Working!');
-})
+app.use('/api', graphqlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }   
+    
+    type RootMutation {
+        createEvent(name: String): String
+    }
+    
+    schema {
+            query: RootQuery 
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Look', 'at', 'the', 'list!']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName
+        }
+    },
+    graphiql: true
+}));
 
-app.listen(3000);
+app.listen(3300);
